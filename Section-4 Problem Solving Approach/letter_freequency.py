e067233c6d69190bf1233fa60b0b0b0b882a23df
Iftakhar Ahmed <iftakharn19@gmail.com>
2020-07-22 22:29:04 +0600

    added section 4

---
+++ Section-4 Problem Solving Approach/letter_freequency.py
@@ -0,0 +1,26 @@
+def is_alpha_numeric(character):
+    if 48 <= ord(character) <= 57: #if character is [0-9]
+        return True
+    elif 97 <= ord(character) <= 122: #if character is [a-z]
+        return True
+    return False
+
+
+def character_count(sentence):
+    character_counts = {} #dict to keep track of character numbers
+    
+    for character in sentence:
+        #converting the character to lower case
+        character = character.lower()
+        
+        #checking if the character is valid for us.
+        if is_alpha_numeric(character): 
+            if character_counts.get(character):
+                character_counts[character] += 1
+            else:
+                character_counts[character] = 1
+
+    return character_counts
+
+#demo
+print(character_count(input("Enter some String:\n")))
